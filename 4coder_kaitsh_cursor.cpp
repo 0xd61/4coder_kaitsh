aff46c151af89dfc4fae983e1af7c4db479c23df
kaitsh <kaitsh@d-git.de>
2020-03-13 18:03:10 -0300

    Use interpolation function of ryanfleury

--- 4coder_kaitsh_cursor.cpp
+++ 4coder_kaitsh_cursor.cpp
@@ -1,3 +1,53 @@
+internal void
+FleuryDoTheCursorInterpolation(Application_Links *app, Frame_Info frame_info,
+                               Rect_f32 *rect, Rect_f32 *last_rect, Rect_f32 target)
+{
+    *last_rect = *rect;
+    
+    float x_change = target.x0 - rect->x0;
+    float y_change = target.y0 - rect->y0;
+    
+    float cursor_size_x = (target.x1 - target.x0);
+    float cursor_size_y = (target.y1 - target.y0) * (1 + fabsf(y_change) / 60.f);
+    
+    b32 should_animate_cursor = true;//!global_battery_saver;
+    if(should_animate_cursor)
+    {
+        if(fabs(x_change) > 1.f || fabs(y_change) > 1.f)
+        {
+            animate_in_n_milliseconds(app, 0);
+        }
+    }
+    else
+    {
+        *rect = *last_rect = target;
+        cursor_size_y = target.y1 - target.y0;
+    }
+    
+    if(should_animate_cursor)
+    {
+        rect->x0 += (x_change) * frame_info.animation_dt * 30.f;
+        rect->y0 += (y_change) * frame_info.animation_dt * 30.f;
+        rect->x1 = rect->x0 + cursor_size_x;
+        rect->y1 = rect->y0 + cursor_size_y;
+    }
+    
+    if(target.y0 > last_rect->y0)
+    {
+        if(rect->y0 < last_rect->y0)
+        {
+            rect->y0 = last_rect->y0;
+        }
+    }
+    else
+    {
+        if(rect->y1 > last_rect->y1)
+        {
+            rect->y1 = last_rect->y1;
+        }
+    }
+    
+}
 
 internal void
 KaitshDrawCursorMarkHighlight(Application_Links *app, View_ID view_id, b32 is_active_view,
@@ -18,39 +68,9 @@ KaitshDrawCursorMarkHighlight(Application_Links *app, View_ID view_id, b32 is_ac
             
             Rect_f32 target_rect = text_layout_character_on_screen(app, text_layout_id, cursor_pos);
             
-            float x_change = target_rect.x0 - rect.x0;
-            float y_change = target_rect.y0 - rect.y0;
-            float cursor_size_x = (target_rect.x1 - target_rect.x0);
-            float cursor_size_y = (target_rect.y1 - target_rect.y0) * (1 + fabsf(y_change) / 60.f);
-            
-            rect.x0 += (x_change) * frame_info.animation_dt * 14.f;
-            rect.y0 += (y_change) * frame_info.animation_dt * 14.f;
-            rect.x1 = rect.x0 + cursor_size_x;
-            rect.y1 = rect.y0 + cursor_size_y;
-            
-            if(target_rect.y0 > last_rect.y0)
-            {
-                if(rect.y0 < last_rect.y0)
-                {
-                    rect.y0 = last_rect.y0;
-                }
-            }
-            else
-            {
-                if(rect.y1 > last_rect.y1)
-                {
-                    rect.y1 = last_rect.y1;
-                }
-            }
-            
-            if(fabs(x_change) > 1.f || fabs(y_change) > 1.f)
-            {
-                animate_in_n_milliseconds(app, 0);
-            }
-            
-            // TODO(dgl): Why is there no rectangle???
+            FleuryDoTheCursorInterpolation(app, frame_info, &rect,
+                                           &last_rect, target_rect);
             draw_rectangle_fcolor(app, rect, roundness, fcolor_id(defcolor_cursor));
-            
             //draw_character_block(app, text_layout_id, cursor_pos, roundness, fcolor_id(defcolor_cursor, cursor_sub_id));
             paint_text_color_pos(app, text_layout_id, cursor_pos,
                                  fcolor_id(defcolor_at_cursor));
