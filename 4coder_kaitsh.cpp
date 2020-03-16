0de2f3e851819586a11c412443f6dee3e763f7ce
kaitsh <kaitsh@d-git.de>
2020-03-16 10:46:24 -0300

    Show file percentage on buffer bar

--- 4coder_kaitsh.cpp
+++ 4coder_kaitsh.cpp
@@ -26,6 +26,66 @@ CUSTOM_DOC("toggle battery saver variable")
 }
 
 internal void
+KaitshDrawFileBar(Application_Links *app, View_ID view_id, Buffer_ID buffer, Face_ID face_id, Rect_f32 bar)
+{
+    Scratch_Block scratch(app);
+    
+    draw_rectangle_fcolor(app, bar, 0.f, fcolor_id(defcolor_bar));
+    
+    FColor base_color = fcolor_id(defcolor_base);
+    FColor pop2_color = fcolor_id(defcolor_pop2);
+    
+    i64 cursor_position = view_get_cursor_pos(app, view_id);
+    Buffer_Cursor cursor = view_compute_cursor(app, view_id, seek_pos(cursor_position));
+    i64 LineCount = buffer_get_line_count(app, buffer);
+    i64 BufferPercent = (i64)(((f32)cursor.line / (f32)LineCount) * 100);
+    
+    Fancy_Line list = {};
+    String_Const_u8 unique_name = push_buffer_unique_name(app, scratch, buffer);
+    push_fancy_string(scratch, &list, base_color, unique_name);
+    push_fancy_stringf(scratch, &list, base_color, " - Row: %3.lld Col: %3.lld (%2lld%) -", cursor.line, cursor.col, BufferPercent);
+    
+    Managed_Scope scope = buffer_get_managed_scope(app, buffer);
+    Line_Ending_Kind *eol_setting = scope_attachment(app, scope, buffer_eol_setting,
+                                                     Line_Ending_Kind);
+    switch (*eol_setting){
+        case LineEndingKind_Binary:
+        {
+            push_fancy_string(scratch, &list, base_color, string_u8_litexpr(" bin"));
+        }break;
+        
+        case LineEndingKind_LF:
+        {
+            push_fancy_string(scratch, &list, base_color, string_u8_litexpr(" lf"));
+        }break;
+        
+        case LineEndingKind_CRLF:
+        {
+            push_fancy_string(scratch, &list, base_color, string_u8_litexpr(" crlf"));
+        }break;
+    }
+    
+    u8 space[3];
+    {
+        Dirty_State dirty = buffer_get_dirty_state(app, buffer);
+        String_u8 str = Su8(space, 0, 3);
+        if (dirty != 0){
+            string_append(&str, string_u8_litexpr(" "));
+        }
+        if (HasFlag(dirty, DirtyState_UnsavedChanges)){
+            string_append(&str, string_u8_litexpr("*"));
+        }
+        if (HasFlag(dirty, DirtyState_UnloadedChanges)){
+            string_append(&str, string_u8_litexpr("!"));
+        }
+        push_fancy_string(scratch, &list, pop2_color, str.string);
+    }
+    
+    Vec2_f32 p = bar.p0 + V2f32(2.f, 2.f);
+    draw_fancy_line(app, face_id, fcolor_zero(), &list, p);
+}
+
+internal void
 KaitshRenderBuffer(Application_Links *app, View_ID view_id, Face_ID face_id,
                    Buffer_ID buffer, Text_Layout_ID text_layout_id,
                    Rect_f32 rect, Frame_Info frame_info)
@@ -157,7 +217,7 @@ KaitshRenderCaller(Application_Links *app, Frame_Info frame_info, View_ID view_i
     b64 showing_file_bar = false;
     if (view_get_setting(app, view_id, ViewSetting_ShowFileBar, &showing_file_bar) && showing_file_bar){
         Rect_f32_Pair pair = layout_file_bar_on_top(region, line_height);
-        draw_file_bar(app, view_id, buffer, face_id, pair.min);
+        KaitshDrawFileBar(app, view_id, buffer, face_id, pair.min);
         region = pair.max;
     }
     
